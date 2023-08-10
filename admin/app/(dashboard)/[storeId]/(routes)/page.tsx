bef20429db33501e4185cd68c0cca2c5f3ac3044
ralph-p <mr.pereira91@gmail.com>
2023-08-10 15:24:28 -0400

    added settings page

--- admin/app/(dashboard)/[storeId]/(routes)/page.tsx
+++ admin/app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,4 +1,6 @@
+import { Heading } from "@/components/ui/heading";
 import prismadb from "@/lib/prismadb";
+import { redirect } from "next/navigation";
 
 interface DashboardPageProps {
   params: {
@@ -9,7 +11,14 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
   const store = await prismadb.store.findFirst({
     where: { id: params.storeId },
   });
-  return <div>Dashboard Page for store {store?.name}</div>;
+  if (!store) redirect("/");
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <Heading title={store?.name} description="Store Dashboard" />
+      </div>
+    </div>
+  );
 };
 
 export default DashboardPage;
